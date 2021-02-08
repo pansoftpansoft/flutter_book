a26ca4c18e0d05c1a2e066f71f10843c53fefdeb
pansoftpansoft <pansoft@bk.ru>
2021-02-08 22:31:55 +0300

    Добавлен файлы Appointment.dart

--- lib/appointments/Appointments.dart
+++ lib/appointments/Appointments.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'AppointmentsDBWorker.dart';
+import 'AppointmentsModel.dart' show AppointmentsModel, appointmentsModel;
 
 class Appointments extends StatelessWidget {
   Appointments() {
-    print('Appointments');
+    appointmentsModel.loadData("appointments", AppointmentsDBWorker.db);
   }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container();
+    throw UnimplementedError();
   }
 }
